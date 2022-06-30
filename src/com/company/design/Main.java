ba7de582123baa57650e61d46abaf6206507eea9
kunholee_98 <dlrjsgh750@naver.com>
2022-06-30 16:05:03 +0900

    observer pattern

--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -3,6 +3,8 @@ package com.company.design;
 import com.company.design.adapter.*;
 import com.company.design.aop.AopBrowser;
 import com.company.design.decorator.*;
+import com.company.design.observer.Button;
+import com.company.design.observer.IButtonListener;
 import com.company.design.proxy.BrowserProxy;
 import com.company.design.proxy.IBrowser;
 
@@ -11,19 +13,21 @@ import java.util.concurrent.atomic.AtomicLong;
 public class Main {
 
     public static void main(String[] args) {
-        ICar audi = new Audi(1000);
-        audi.showPrice();
+        Button button = new Button("버튼");
 
-        // a3
-        ICar audi3 = new A3(audi, "A3");
-        audi3.showPrice();
-        // a4
-        ICar audi4 = new A4(audi, "A4");
-        audi4.showPrice();
-        // a5
-        ICar audi5 = new A5(audi, "A5");
-        audi5.showPrice();
+        button.addListener(new IButtonListener() {
+            @Override
+            public void clickEvent(String event) {
+                System.out.println(event);
+            }
+        });
+
+        button.click("click 1");
+        button.click("click 2");
+        button.click("click 3");
+        button.click("click 4");
     }
+
 
     public static void connect(Electronic110V electronic110V){
         electronic110V.powerOn();
