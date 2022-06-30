44b0fdb858e37826ecdbfb1f72e870006cbf22a9
kunholee_98 <dlrjsgh750@naver.com>
2022-06-30 15:29:33 +0900

    proxy & aop pattern

--- src/com/company/design/Main.java
+++ src/com/company/design/Main.java
@@ -1,34 +1,39 @@
 package com.company.design;
 
 import com.company.design.adapter.*;
-import com.company.design.singleton.Aclazz;
-import com.company.design.singleton.Bclazz;
-import com.company.design.singleton.SocketClient;
+import com.company.design.aop.AopBrowser;
+import com.company.design.proxy.BrowserProxy;
+import com.company.design.proxy.IBrowser;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 public class Main {
 
     public static void main(String[] args) {
+//        Browser browser = new Browser("www.ohou.se");
+//        browser.show();
+//        IBrowser browser = new BrowserProxy("www.ohou.se");
+//        browser.show();
+//        browser.show();
+//        browser.show();
+//        browser.show();
 
-//        Aclazz aClazz = new Aclazz();
-//        Bclazz bClazz = new Bclazz();
-//
-//        SocketClient aClient = aClazz.getSocketClient();
-//        SocketClient bClient = bClazz.getSocketClient();
-//
-//        System.out.println("두개의 객체가 동일한가?");
-//        System.out.println(aClient.equals(bClient));
+        AtomicLong start = new AtomicLong();
+        AtomicLong end = new AtomicLong();
 
-        HairDryer hairDryer = new HairDryer();
-        connect(hairDryer);
+        IBrowser aopBrowser = new AopBrowser("www.ohou.se",
+                ()->{
+                    System.out.println("before");
+                    start.set(System.currentTimeMillis());
+                },
+                ()->{
+                    long now = System.currentTimeMillis();
+                    end.set(now - start.get());
+                }
+            );
+        aopBrowser.show();
+        System.out.println("loading time: "+end.get());
 
-        Cleaner cleaner = new Cleaner();
-
-        Electronic110V adapter = new SocketAdapter(cleaner);
-        connect(adapter);
-
-        AirConditioner airConditioner = new AirConditioner();
-        Electronic110V airAdapter = new SocketAdapter(airConditioner);
-        connect(airAdapter);
 
     }
 
